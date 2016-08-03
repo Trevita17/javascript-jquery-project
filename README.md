95d210f8500bf6fd04c5c5c819b8f98c288163de
Trevor Long <Trevordlong@gmail.com>
2016-08-03 00:31:28 -0500

    update README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Project: Javascript and JQuery
 
 http://www.theodinproject.com/web-development-101/javascript-and-jquery?ref=lnav
+
+I've managed to create a crude sketchpad using javascript and jQuery. I wanted to include the ability to create up to 128x128 box grids within a 960 pixel space. With my current working knowledge, any solution I found caused the page to run too slowly. 32x32 is workable if you're a little patient. I've left some commands for buttons in the code, as I'm planning to come back later and implement new features as my knowledge expands (using the buttons as opposed to refreshing the page caused a load time of more than a minute for 64x64 grids). For now, refresh the page and choose a new value to load a new sketchpad. 
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,41 @@
 $(function() {
+  var input = prompt('Choose "thick" or "thin". Refresh the page to start again.')
+  if (input === "thick") {
     for (var x = 0; x < 16; x++) {
-        for (var y = 0; y < 16; y++) {
-            $("<div>").addClass("unit").appendTo("#container");
-	$('.unit').hover(function(){
-	$(this).addClass('highlight');
-	$('button').click(function(){
-	$('.unit').removeClass('highlight');
-});
-});
-        }
+      for (var y = 0; y < 16; y++) {
+        $("<div>").addClass("unit1").appendTo("#container");
+        $('.unit1').mouseenter(function() {
+          $(this).addClass('highlight');
+          $('#button1').click('input', function() {
+            $('.unit1').removeClass('highlight');
+          });
+        });
+      }
     }
-
+  } else if (input === "thin") {
+    for (var x = 0; x < 32; x++) {
+      for (var y = 0; y < 32; y++) {
+        $("<div>").addClass("unit2").appendTo("#container");
+        $('.unit2').mouseenter(function() {
+          $(this).addClass('highlight');
+          $('#button1').click('input', function() {
+            $('.unit2').removeClass('highlight');
+          });
+        });
+      }
+    }
+  } else if (input === "64") {
+    for (var x = 0; x < 64; x++) {
+      for (var y = 0; y < 64; y++) {
+        $("<div>").addClass("unit3").appendTo("#container");
+        $('.unit3').mouseenter(function() {
+          $(this).addClass('highlight');
+          $('#button1').click('input', function() {
+            $('.unit3').removeClass('highlight');
+          });
+        });
+      }
+    }
+  }
 });
+
